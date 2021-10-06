143a45a1faae4b226fa55f7abdd3bbe1554a9824
Marcel Esteves <marcel.esteves@zpesystems.com>
2021-10-06 20:08:59 -0300

    adding lint-staged

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
-const WebpackShellPluginNext = require("webpack-shell-plugin-next");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 const NODE_ENV =
-  process.env.NODE_ENV === "production" ? "production" : "development";
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
   mode: NODE_ENV,
-  watch: NODE_ENV === "development",
-  entry: path.resolve(__dirname, "src/main.js"),
+  watch: NODE_ENV === 'development',
+  entry: path.resolve(__dirname, 'src/main.js'),
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
   },
   resolve: {
-    extensions: [".js", ".json"],
+    extensions: ['.js', '.json'],
     alias: {
-      "@Domains": path.resolve(__dirname, "src/domains"),
-      "@Core": path.resolve(__dirname, "src/core"),
-      "@": path.resolve(__dirname, "src"),
+      '@Domains': path.resolve(__dirname, 'src/domains'),
+      '@Core': path.resolve(__dirname, 'src/core'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  target: "node",
+  target: 'node',
   module: {
     rules: [
       {
         test: /.js/,
-        exclude: path.resolve(__dirname, "src/Core/App/Mongo-init.js"),
+        exclude: path.resolve(__dirname, 'src/Core/App/Mongo-init.js'),
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
@@ -40,9 +40,9 @@ module.exports = {
   externals: [nodeExternals()],
   plugins: [
     new WebpackShellPluginNext({
-        onBuildExit: {
-          scripts: NODE_ENV === "development" ? ["supervisor build/index.js"] : [],
-          parallel: true,
+      onBuildExit: {
+        scripts: NODE_ENV === 'development' ? ['supervisor build/index.js'] : [],
+        parallel: true,
       }
     }),
   ],
